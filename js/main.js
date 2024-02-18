d74c467af84a680f5a4f5a0d49b8d47db272b187
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-02-18 14:49:51 +0600

    Scroll To Fixed Header

--- js/main.js
+++ js/main.js
@@ -3,5 +3,21 @@ const menuToggle = document.getElementById("menu-toggle");
 menuToggle.addEventListener("click", () => {
   const mobileTabletMenu = document.getElementById("mobile-tablet-menu");
   mobileTabletMenu.classList.toggle("show-menu");
-  console.log(mobileTabletMenu);
+  menuToggle.classList.toggle("text-green");
 });
+
+// Fixed nav
+window.onscroll = function () {
+  fixedNav();
+};
+const nav = document.getElementById("nav");
+const navbar = document.getElementById("navbar");
+function fixedNav() {
+  if (window.pageYOffset > 50) {
+    nav.classList.add("sticky");
+    navbar.classList.add("!py-6");
+  } else {
+    nav.classList.remove("sticky");
+    navbar.classList.remove("!py-6");
+  }
+}
